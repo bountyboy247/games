9444a6df18acaef7ae3ab5fb66fc205a137729e7
bountyboy247 <bountyboy247@outlook.com>
2023-02-22 20:26:06 -0700

    added logic to move ball manually using keyboard

--- main.py
+++ main.py
@@ -30,6 +30,10 @@ FRAMES_PER_SECOND = 30
 BALL_WIDTH_HEIGHT = 60
 MAX_WIDTH = WINDOW_WIDTH - BALL_WIDTH_HEIGHT
 MAX_HEIGHT = WINDOW_HEIGHT - BALL_WIDTH_HEIGHT
+TARGET_X = 400
+TARGET_Y = 320
+TARGET_WIDTH_HEIGHT = 120
+N_PIXELS_TO_MOVE = 3
 #3 Initialize the world
 
 pygame.init()
@@ -38,11 +42,14 @@ clock = pygame.time.Clock()
 
 #4 -> Load assets: image(s), sound(s), etc.
 ballImage = pygame.image.load('images/ball.png')
+targetImage = pygame.image.load('images/target.png')
 ballImageSmall = pygame.transform.scale(ballImage,(BALL_WIDTH_HEIGHT,BALL_WIDTH_HEIGHT))
+targetImageSmall = pygame.transform.scale(targetImage,(TARGET_WIDTH_HEIGHT,TARGET_WIDTH_HEIGHT))
 #5 -Initialize variables
 ballx = random.randrange(MAX_WIDTH)
 bally = random.randrange(MAX_HEIGHT)
-ballRect = pygame.Rect(ballx,bally,BALL_WIDTH_HEIGHT,BALL_WIDTH_HEIGHT)
+#ballRect = pygame.Rect(ballx,bally,BALL_WIDTH_HEIGHT,BALL_WIDTH_HEIGHT)
+targetRect = pygame.Rect(TARGET_X,TARGET_Y, TARGET_WIDTH_HEIGHT,TARGET_WIDTH_HEIGHT)
 #6-LoopForever
 while True:
     #7 check for and handle events
@@ -52,18 +59,26 @@ while True:
             pygame.quit()
             sys.exit()
         #see if user clicked
-        if event.type == pygame.MOUSEBUTTONUP:
-            if ballRect.collidepoint(event.pos):
-                ballx = random.randrange(MAX_WIDTH)
-                bally = random.randrange(MAX_HEIGHT)
-                ballRect = pygame.Rect(ballx,bally, BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                ballx -= N_PIXELS_TO_MOVE
+            elif event.key == pygame.K_RIGHT:
+                ballx += N_PIXELS_TO_MOVE
+            elif event.key == pygame.K_UP:
+                bally -= N_PIXELS_TO_MOVE
+            elif event.key == pygame.K_DOWN:
+                bally += N_PIXELS_TO_MOVE
     #8 do any "per frame" actions
-
+    ballRect = pygame.Rect(ballx,bally, BALL_WIDTH_HEIGHT,BALL_WIDTH_HEIGHT)
+    
+    if ballRect.colliderect(targetRect):
+        print('Ball is touching the target')
     #9 clear the window
 
     window.fill(WHITE)
 
     #10 Draw all window elements
+    window.blit(targetImageSmall,(TARGET_X,TARGET_Y))
     window.blit(ballImageSmall,(ballx,bally)) #bit block transfer, just means draw
     #11 update the window
     pygame.display.update()
